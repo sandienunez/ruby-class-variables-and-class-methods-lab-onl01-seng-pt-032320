64e516a4a6230a2f95d21de5bb1930bf66c9306d
Sandie Nunez <snunez@vt.edu>
2020-04-16 04:38:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
-class Song
+#class Song
+  
 #   attr_accessor :name, :artist, :genre
 
 #   def initialize(name, artist, genre)
@@ -8,3 +9,64 @@ class Song
 #   @@count = 0
 #   end 
 # end   
+
+ class Song
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+   @@count = 0
+  end 
+end   
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(song_name, artist, genre)
+    @artist = artist
+    @name = song_name
+    @@genres << genre
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else 
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+end
